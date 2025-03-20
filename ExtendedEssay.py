24c9542a4ae566fdfb73b528c93be3ec13a7e14f
Jason Holt <you@example.com>
2025-03-19 23:26:27 -0400

    Set-up CNN model with layers and data

--- ExtendedEssay.py
+++ ExtendedEssay.py
@@ -1,6 +1,43 @@
 import tensorflow as tf
+from tensorflow import keras
 
-from tensorflow.keras import datasets, layers, models
-import matplotlib.pyplot as plt
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras import models
+from tensorflow.python.keras import layers
+from matplotlib import pyplot as plt
 
+from tensorflow.keras.datasets import mnist
+
+# Data Set-up
+img_rows, img_cols = 28, 28
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+train_images = train_images.reshape(train_images.shape[0], img_rows, img_cols, 1)
+test_images = test_images.reshape(test_images.shape[0], img_rows, img_cols, 1)
+input_shape = (img_rows, img_cols, 1)
+
+train_images = train_images.astype('float32')
+test_images = test_images.astype('float32')
+train_images /= 255
+test_images /= 255
+
+train_labels = keras.utils.to_categorical(train_labels, 10)
+test_labels = keras.utils.to_categorical(test_labels, 10)
+
+# Model set-up
+
+epochs = 10
 model = models.Sequential()
+model.add(layers.Conv2D(32,(3,3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
+model.compile(optimizer='adam', loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
+history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
+
+model.summary()
+
+# Plotting image

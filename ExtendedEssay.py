72a8154ed011d7dfff5f5707c9cafca853322456
JasonH81 <jasontholt81@gmail.com>
2025-03-20 00:21:56 -0400

    Added data augmentation testing

--- ExtendedEssay.py
+++ ExtendedEssay.py
@@ -11,9 +11,6 @@ img_rows, img_cols = 28, 28
 train_images = train_images.reshape(train_images.shape[0], img_rows, img_cols, 1)
 test_images = test_images.reshape(test_images.shape[0], img_rows, img_cols, 1)
 
-
-input_shape = (img_rows, img_cols, 1)
-
 train_images = train_images.astype('float32')
 test_images = test_images.astype('float32')
 train_images /= 255
@@ -22,10 +19,16 @@ test_images /= 255
 train_labels = keras.utils.to_categorical(train_labels, 10)
 test_labels = keras.utils.to_categorical(test_labels, 10)
 
-# Model set-up
+# Data augmentation
+data_augmentation = tf.keras.Sequential([
+    layers.RandomFlip("horizontal_and_vertical"),
+    layers.RandomRotation(0.2),
+])
 
+# Model set-up
 epochs = 10
 model = models.Sequential()
+#model.add(data_augmentation)
 model.add(layers.Conv2D(32,(3,3), activation='relu', input_shape=(28, 28, 1)))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
@@ -39,3 +42,7 @@ model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accur
 model.fit(train_images, train_labels, batch_size=64, epochs=epochs, validation_data=(test_images, test_labels), shuffle=True)
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 print(f"Test Loss: {test_loss}, Test Accuracy: {test_acc}")
+
+#plt.imshow(test_images[0].reshape(28, 28), cmap='gray')
+#plt.title(f"Predicted label: {model.predict(test_images[0:1]).argmax()}")
+#plt.show()
